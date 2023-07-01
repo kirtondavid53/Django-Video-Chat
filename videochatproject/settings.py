98b58260a7a99617826f018e24df63b0822a89ce
kirtondavid53 <kirtondavid53@gmail.com>
2023-07-01 17:27:20 -0400

    requirements.txt

--- videochatproject/settings.py
+++ videochatproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
